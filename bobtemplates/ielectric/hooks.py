12a69d5d7a62b4a39a75dfa4ced4e0b845c71ab7
Daniel Jowett <daniel@jowettenterprises.com>
2014-05-29 14:19:26 +0100

    Also remove five:registerPackage & Extensions directory if profile is not required

--- bobtemplates/ielectric/hooks.py
+++ bobtemplates/ielectric/hooks.py
@@ -2,16 +2,24 @@
 import os
 import shutil
 
-def delete_profile(configurator):
-    deldir = os.path.join (configurator.target_directory, 
-                           configurator.variables['package.namespace'],
-                           configurator.variables['package.name'],
-                           'profiles')
+def delete_profile_stuff(configurator):
+    package_dir = os.path.join (configurator.target_directory, 
+                                configurator.variables['package.namespace'],
+                                configurator.variables['package.name'])
+    profiles_dir = os.path.join (package_dir,
+                                 'profiles')
+    extensions_dir = os.path.join (package_dir,
+                                   'Extensions')
+                            
+    delete_dirtree(configurator, profiles_dir)
+    delete_dirtree(configurator, extensions_dir)
+
+def delete_dirtree(configurator, directory):
     if configurator.variables['genericsetup.profile']:
-        print 'keeping ' + deldir
+        print 'keeping ' + directory
     else:
-        print 'deleting ' + deldir
-        shutil.rmtree(deldir)
-        if os.path.exists(deldir):
-            print "Couldn't remove " + deldir
+        print 'deleting ' + directory
+        shutil.rmtree(directory)
+        if os.path.exists(directory):
+            print "Couldn't remove " + directory
 
